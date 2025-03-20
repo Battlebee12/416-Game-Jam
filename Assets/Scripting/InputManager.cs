a6b64c9830c69f7b5d0d00b5a694d45bb4125d56
Battlebee12 <Sarab4space@gmail.com>
2025-03-19 23:18:00 -0700

    Implemented 2 player Movement
    you can drop the two players into the scene and. in the input manager you can select which one is player2.

--- Assets/Scripting/InputManager.cs
+++ Assets/Scripting/InputManager.cs
@@ -2,22 +2,42 @@ using UnityEngine;
 
 public class InputManager : MonoBehaviour
 {
+
+    [SerializeField] private bool isplayer2 = false;
+
+    private KeyCode jump = KeyCode.W;
+    private KeyCode shoot = KeyCode.F;
+    private KeyCode right = KeyCode.D;
+    private KeyCode left = KeyCode.A;
+
+
+    private void Start()
+    {
+        if(isplayer2){
+            jump = KeyCode.UpArrow;
+            shoot = KeyCode.RightControl;
+            right = KeyCode.RightArrow;
+            left = KeyCode.LeftArrow;
+            
+        }
+        
+    }
     public int GetMovement2d(){
         int movement_Int =0;
-        if(Input.GetKey(KeyCode.A)){
+        if(Input.GetKey(left)){
             movement_Int = -1;
         }
-        if(Input.GetKey(KeyCode.D)){
+        if(Input.GetKey(right)){
             movement_Int = 1;
         }
         return movement_Int;
     }
 
     public bool GetJumping2d(){
-        return Input.GetKey(KeyCode.Space);
+        return Input.GetKey(jump);
     }
 
     public bool IsShooting(){
-        return Input.GetKeyDown(KeyCode.F);
+        return Input.GetKeyDown(shoot);
     }
 }
